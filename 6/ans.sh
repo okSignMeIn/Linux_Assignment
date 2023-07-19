a969e2662ed38f40e88faea071ed22f56853fc6b
Lakshya <lakshyagupta87@yahoo.com>
2023-07-19 12:38:13 +0530

    modifed ans 6

--- 6/ans.sh
+++ 6/ans.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
+#!/bin/bash
 
-find /home/shtlp_0039 -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" \) -size +10M
-
+find /home/shtlp_0039 -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.mp4" \) -size +10k -exec du -h {} \; | sort -rh
